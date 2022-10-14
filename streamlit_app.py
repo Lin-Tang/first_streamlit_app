08d92daaf83bf64ad7f7a58b9f5e5023ca22655d
Lin-Tang <58560349+Lin-Tang@users.noreply.github.com>
2022-10-15 01:02:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,5 +33,3 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.multiselect("What fruit would you like to add?:", list(my_fruit_list.index), ['Jackfruit'])
-thanks_notes = "Thanks for adding" + add_my_fruit
-streamlit.text(thanks_notes)
